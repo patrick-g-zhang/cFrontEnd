1826d33b706a3c76cd8859c69326a01d13d0cd6e
guangyanee@gmail.com <Frank@0724>
2019-10-03 16:30:33 +0800

    fixed typo of mandarin front end

--- src/casia_test.py
+++ src/casia_test.py
@@ -1,211 +1,125 @@
-import os
-import glob
-import logging
-from pypinyin import pinyin, Style
-from jieba import posseg
+from corpus.casia import Casia
+from FrontEnd import FrontEnd
 import re
+import os
+from praatio import tgio
+import pdb
 from collections import OrderedDict
 from linguistic_dict import Linguistic_DICT
 ld = Linguistic_DICT()
 
 
-class MFrontEnd(object):
-    def __init__(self, text_file, file_id_path='./gen/file_id_list_kingtts.scp'):
-        self.text_file = text_file
-        self.file_id_path = file_id_path
-        self.label_phone_align = './label_phone_align/'
+class casia_front(FrontEnd):
+    """docstring for ClassName"""
 
-        logger = logging.getLogger('creat front end for chinese')
+    def __init__(self, ):
+        super(FrontEnd, self).__init__()
+        # here we will only do with liuchanhg
+        self.alignment_file_dir = "/home/gyzhang/Documents/aligned_liuchanhg"
+        self.label_phone_align = "/home/gyzhang/projects/cFrontEnd/exp/casia/train/label_phone_align"
+        os.makedirs(self.label_phone_align, exist_ok=True)
+        self.file_id_path = "/home/gyzhang/projects/cFrontEnd/exp/casia/train/file_id_list.scp"
+        self.chinese_dict = ld.get_lexicon_dict(
+            lexicon_path="../data/dicts/mandarin_syl2phoneme.txt")
 
-    def pos_phone_in_syl(self, phone_index, syl_map):
-        syl_index = syl_map[phone_index]
-        syl_index_list = []
-        for key, value in syl_map.items():
-            if value == syl_index:
-                syl_index_list.append(key)
-        syl_len = len(syl_index_list)
-        fw_pos = syl_index_list.index(phone_index) + 1
-        bw_pos = syl_len - fw_pos + 1
-        return fw_pos, bw_pos, syl_index, syl_len
+      #  ss = Casia()
+      #  ss.process_same_text()
 
-    def get_phone_num_of_syl(self, syl_index, syl_map):
-        syl_index_list = []
-        for key, value in syl_map.items():
-            if value == syl_index:
-                syl_index_list.append(key)
-        syl_len = len(syl_index_list)
-        return syl_len
-
-    def pos_syl_in_word(self, char_index, word_map):
-        word_index = word_map[char_index]
-        word_index_list = []
-        for key, value in word_map.items():
-            if value == word_index:
-                word_index_list.append(key)
-        word_len = len(word_index_list)
-        fw_syl_pos = word_index_list.index(char_index) + 1
-        bw_syl_pos = word_len - fw_syl_pos + 1
-        return fw_syl_pos, bw_syl_pos, word_index, word_len
-
-    def get_syl_num_of_word(self, word_index, word_map):
-        word_index_list = []
-        for key, value in word_map.items():
-            if value == word_index:
-                word_index_list.append(key)
-        word_len = len(word_index_list)
-        return word_len
-
-    def create_utt_spk(wav_folder="/home/gyzhang/speech_database/1001", utt2spk="./gen/utt2spk"):
+    def align_phone_map(self, ali_phone_list):
         """
-        Create utt2spk file for kaldi project
-
-        Arguments:
-        wav_folder -- folder with wave files
-
+            because alignment file contains 'sil' and 'sp' two symbols which we must need to remove
+            we return a file which contains a list contains non-silence index
+            [1,2,3,5]
+        :param ali_phone_list:
+        :return:
         """
+        non_silence_index_list = []
+        for num, phone in enumerate(ali_phone_list):
+            if phone != "sil" and phone != "sp":
+                non_silence_index_list.append(num)
+        return non_silence_index_list
 
-        f_out = open(utt2spk, 'w')
-
-        for file in glob.glob(wav_folder + '/*.wav'):
-            base = os.path.basename(file).split('.')[0]
-            f_out.write(base + '\t' + 'tts' + '\n')
-
-    def get_word_pos_list(self, raw_text):
+    def check_phone_consistent(self, ):
         """
-            get word and pos lists from raw text
-
-        Parameters:
-            raw_text -- raw input as a sentence in our corpus
-        Returns:
-            word_list -- list of words in a sentence
-            pos_list -- list of pos in a sentence
+            make sure phone same in 
         """
-        raw_text = raw_text.strip()
-        word_list = []
-        pos_list = []
-        seg_list = posseg.cut(raw_text, HMM=False)  # 默认是精确模式
-        for word, flag in seg_list:
-            # remove the punctuation
-            if word in ['。', '？', '：', '、', '…', '；', '，']:
-                continue
-            word_list.append(word)
-            pos_list.append(flag)
-        return word_list, pos_list
-
-    def get_word_phone_list(self, word_list):
-        """
-            get phone list and phone array of word_list
-
-        Parameters:
-            word_list -- list of words in a sentence ["我"，"是个"]
-            create_oov -- oov dictionary needed to be created at first step.
-        """
-
-        phone_list = []
-        tone_list = []
-        syl_map = OrderedDict()
-        word_map = OrderedDict()
-        phone_index = 0
-        char_index = 0
-        word_index = 0
-        ipa_dict = ld.get_lexicon_dict(lexicon_path='./dicts/ipa_m.dict')
-        for word in word_list:
-            word_pinyin_initials = pinyin(word, style=Style.INITIALS)
-            word_pinyin_finals = pinyin(word, style=Style.FINALS)
-            word_pinyin_tone = pinyin(word, style=Style.TONE3)
-            assert len(word_pinyin_initials) == len(word_pinyin_finals)
-            for initial, final, all_tone in zip(word_pinyin_initials, word_pinyin_finals, word_pinyin_tone):
-                char_phone_list = []
-                assert len(initial) == 1
-                assert len(final) == 1
-                if not initial[0] == "":
-                    ipa_initial = ipa_dict[initial[0]]
-                    char_phone_list.extend(ipa_initial)
-                if not final[0] == "":
-                    try:
-                        ipa_final = ipa_dict[final[0]]
-                        char_phone_list.extend(ipa_final)
-                    except KeyError:
-                        print(word)
-                        exit(0)
-                else:
-                    print("error, the final is empty")
-                    exit(0)
-                phone_list.extend(char_phone_list)
-                for my_phone in char_phone_list:
-                    syl_map[phone_index] = char_index
-                    phone_index = phone_index + 1
-                if all_tone[0][-1].isdigit():
-                    tone_list.append(all_tone[0][-1])
-                else:
-                    tone_list.append('5')
-                word_map[char_index] = word_index
-                char_index = char_index + 1
-            word_index = word_index + 1
-        #     logging.debug("phone_list:" + ' '.join(phone_list))
-        return phone_list, tone_list, syl_map, word_map
-
-    def pre_process(self, raw_text):
-        """
-        input raw text
-        and output line
-        """
-        # remove the space or other symbols
-        word_lists = re.split(r'\s+', raw_text.strip())
-        if len(word_lists) < 2:
-            print(word_lists)
-            exit(1)
-        sent_index = word_lists[0]
-        word_lists = word_lists[1]
-        word_lists = re.split(r'#', word_lists)
-        sent_content = ''.join(word_lists)
-        return sent_index, sent_content
-
-    def create_file_for_kaldi(self, ):
-        with open(self.text_file, 'r') as fid:
+        with open("../exp/casia/train.txt") as fid:
             textlines = fid.readlines()
-        kaldi_text_no_tone = './gen/no_tone_kaldi_text'
-        kfid = open(kaldi_text_no_tone, 'w')
         for text_line in textlines:
-            sent_index, sent_content = self.pre_process(text_line)
-            word_list, pos_list = self.get_word_pos_list(sent_content)
-            # phone_list, tone_list, syl_map, word_map = self.get_word_phone_list(word_list)
-            phone_list, tone_list, syl_map, word_map = self.get_word_phone_list(
-                word_list)
-            non_tone_line = sent_index + ' ' + ' '.join(phone_list) + '\n'
-            kfid.write(non_tone_line)
+            wav_path, sent_content = re.split("\|", text_line.strip())
+            sent_index = re.split("\.", os.path.basename(wav_path))[0]
+            spk = re.split('\-', sent_index)[0]
+            if spk != "liuchanhg":
+                continue
+            # corresponding label file
+
+            # extract word and pos tagging
+            if "483" in sent_index:
+                continue
+            word_list, pos_list = self.get_word_pos_list(
+                sent_content.strip(), "jieba")
+            phone_list, tone_list, syl_map, word_map, _ = self.get_word_phone_list(
+                word_list, True)
+            phone_list_2 = []
+            # extract alignment file
+            alignment_file_path = os.path.join(
+                self.alignment_file_dir, sent_index + ".TextGrid")
+            tg = tgio.openTextgrid(alignment_file_path)
+            wordTier = tg.tierDict['phones']
+            for start, stop, label in wordTier.entryList:
+                phone = label
+                if phone == "sp" or phone == "sil":
+                    continue
+                phone_list_2.append(phone)
+            assert phone_list_2 == phone_list
+            # replace sp to sil
 
     def create_phone_labels(self, training_data):
-        with open(self.text_file, 'r') as fid:
+        with open("../exp/casia/train.txt") as fid:
             textlines = fid.readlines()
+
         file_id_canto = open(self.file_id_path, 'w')
         for text_line in textlines:
-            sent_index, sent_content = self.pre_process(text_line)
+            wav_path, sent_content = re.split("\|", text_line.strip())
+            sent_index = re.split("\.", os.path.basename(wav_path))[0]
+            spk = re.split('\-', sent_index)[0]
+            if spk != "liuchanhg":
+                continue
+            if "483" in sent_index:
+                continue
+            # corresponding label file
+
             # extract word and pos tagging
-            word_list, pos_list = self.get_word_pos_list(sent_content)
-            phone_list, tone_list, syl_map, word_map = self.get_word_phone_list(
-                word_list)
+            word_list, pos_list = self.get_word_pos_list(
+                sent_content.strip(), "jieba")
+            phone_list, tone_list, syl_map, word_map, _ = self.get_word_phone_list(
+                word_list, True)
             file_id_canto.write(sent_index + '\n')
             sil_phone_list = []
             start_time_list = []
             end_time_list = []
             if training_data:
                 # extract alignment file
-                ali_file = './align_phone/cup_phone_ali/' + sent_index + '.txt'
-                with open(ali_file, 'r') as fid:
-                    ali_file_lines = fid.readlines()
-                for line in ali_file_lines:
-                    line = line.strip()
-                    line_list = re.split('\s+', line)
-                    start_time = str(
-                        int(round(float(line_list[0]), 3) * 10000000))
-                    end_time = str(
-                        int(round(float(line_list[1]), 3) * 10000000))
-                    phone = line_list[2]
-                    phone = re.split('_', phone)[0]
+                alignment_file_path = os.path.join(
+                    self.alignment_file_dir, sent_index + ".TextGrid")
+                tg = tgio.openTextgrid(alignment_file_path)
+                wordTier = tg.tierDict['phones']
+                for start, stop, label in wordTier.entryList:
+                    start_time = str(int(start * 10000000))
+                    end_time = str(int(stop * 10000000))
+                    phone = label
                     sil_phone_list.append(phone)
                     start_time_list.append(start_time)
                     end_time_list.append(end_time)
+                    # replace sp to sil
+                replace_sil_phone_list = []
+                for my_sil_phone in sil_phone_list:
+                    if my_sil_phone == "sp":
+                        replace_sil_phone_list.append("sil")
+                    else:
+                        replace_sil_phone_list.append(my_sil_phone)
+                sil_phone_list = replace_sil_phone_list
+
             else:
                 sil_phone_list = phone_list[:]
                 sil_phone_list.insert(0, 'sil')
@@ -216,7 +130,8 @@ class MFrontEnd(object):
                 if sil_phone != 'sil':
                     sil_nonsil_map[sil_index] = non_sil_index
                     non_sil_index = non_sil_index + 1
-            label_phone_path = self.label_phone_align + sent_index + '.lab'
+            label_phone_path = os.path.join(
+                self.label_phone_align, sent_index + '.lab')
             label_phone_fid = open(label_phone_path, 'w')
             non_sil_len = len(phone_list)
             all_syl_len = len(tone_list)
@@ -341,9 +256,6 @@ class MFrontEnd(object):
         file_id_canto.close()
 
 
-if __name__ == '__main__':
-    mf = MFrontEnd(text_file='./inp/texts.txt',
-                   file_id_path='./gen/file_id_list_kingtts.scp')
-    # mf.create_wav_file()
-    # mf.create_file_for_kaldi()
-    mf.create_phone_labels(True)
+cf = casia_front()
+cf.create_phone_labels(True)
+# cf.process_same_text()
